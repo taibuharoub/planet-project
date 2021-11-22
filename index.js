358f85d43e4bc53e28871ba5a12b4d98d40b57cb
Taibu Haroub <haroubtaibu@gmail.com>
2021-11-22 20:06:36 +0300

    Parse CSV file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
 
 const results = [];
 
+//create a readable stream allows to open a file as a stream
+
 fs.createReadStream("kepler_data.csv")
+  .pipe(parse({ comment: "#", columns: true }))
   .on("data", (data) => {
     results.push(data);
   })
