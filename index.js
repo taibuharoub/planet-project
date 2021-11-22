036cbb2b427f1cfbd167a92d0c65a88addcfa377
Taibu Haroub <haroubtaibu@gmail.com>
2021-11-22 20:23:53 +0300

    Filter out habitable planets

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const fs = require("fs");
 
 const { parse } = require("csv-parse");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  // return planet["koi_disposition"] === "CONFIRMED" && planet["koi_prad"] > 0.5;
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 //create a readable stream allows to open a file as a stream
-
 fs.createReadStream("kepler_data.csv")
   .pipe(parse({ comment: "#", columns: true }))
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("close", () => {
-    console.log(results);
+    console.log(`${habitablePlanets.length} habitable planets found!`);
     console.log("done");
   });
